c12235bddfd02ff10f825b2a021a8077d6a8093e
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-16 15:57:34 +0630

    Ch8-Ep51-dynamic route example in react router(blog detail)

---
+++ src/pages/BlogDetail.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+
+function BlogDetail(){
+
+    let params=useParams();
+    let url="http://localhost:3001/blogs/"+params.id;
+
+    let {data:blog,loading,error}=useFetch(url);
+
+    return (
+        <div>
+            {error && <div>{error}</div>}
+            {loading && <div>Loading ...</div>}
+            {blog && (
+                <div>
+                    <h2>{blog.title}</h2>
+                    <p>Posted By - {blog.author}</p>
+                    <p>{blog.body}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+export default BlogDetail;
